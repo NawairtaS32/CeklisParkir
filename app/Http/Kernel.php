ca22a100fa15872f9e5b6b8917d170789a8387bb
NawairtaS32 <snawairtas@gmail.com>
2022-07-22 15:22:56 +0700

    aplikasi versi 2

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'checkJabatan' => \App\Http\Middleware\CheckJabatan::class,
+        'checkJabatan' => \App\Http\Middleware\checkJabatan::class,
     ];
 }
